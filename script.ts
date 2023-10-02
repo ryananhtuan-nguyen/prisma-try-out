9e8f9268f84b5e7a33ca4a165c99336915f17b24
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-10-02 15:06:56 +1300

    First migration

--- script.ts
+++ script.ts
@@ -1,2 +1,17 @@
 import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
+
+async function main() {
+  const user = await prisma.user.findMany()
+  console.log(user)
+}
+
+try {
+  main()
+} catch (error) {
+  console.error(error)
+} finally {
+  ;async () => {
+    await prisma.$disconnect()
+  }
+}
